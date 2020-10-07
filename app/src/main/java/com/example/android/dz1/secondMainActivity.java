6600ce4a570c281e64aba2857f2a4faf95de0429
Alexandr <5423410@mail.ru>
2020-10-07 14:48:00 +0300

    пробую второй раз.

---
+++ app/src/main/java/com/example/android/dz1/secondMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.dz1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_main);
+    }
+}
